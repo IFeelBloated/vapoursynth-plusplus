88eb451327e1f72588d1b06df3fdb21d375cc61d
John Doe <nickgray0@gmail.com>
2020-04-13 20:31:40 +0800

    Delete Globals.hxx

--- Globals.hxx
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "VapourSynth.h"
-
-namespace VaporGlobals {
-	auto API = static_cast<const VSAPI*>(nullptr);
-	auto Identifier = "";
-	auto Namespace = "";
-	auto Description = "";
-}
